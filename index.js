449e1fd0c8f230409f41fbf698d84a37f29e139f
aleksarad <alexa.radevich1@gmail.com>
2020-07-07 19:28:04 -0400

    MVP done, starting styling

--- index.js
+++ index.js
@@ -1,10 +1,4 @@
-//add categories - maybe take tom to rethink styling
-//add error catching in fetch -> if not successful, task shouldnt be rendered
-//if i allow users to go back and edit past days, how will i account for day ids?
-
 //display category next to task when render
-
-//change complete to heart
 
 let currentDay = ""
 
@@ -15,6 +9,8 @@ document.addEventListener("DOMContentLoaded", function(){
     deleteEditTask()
 })
 const baseUrl = "http://localhost:3000/"
+
+///** FETCHES TO SERVER **///
 
 function fetchDays() {
     fetch(`${baseUrl}days`)
@@ -38,85 +34,6 @@ function fetchCategories() {
     })
 }
 
-function renderCategories(categories) {
-    const categorySelect = document.querySelector("#category")
-    categories.forEach(function(category){
-        const categoryOption = document.createElement("option")
-        categoryOption.textContent = category.name
-        categoryOption.setAttribute("data-id" , `${category.id}`);
-        categorySelect.append(categoryOption)
-    })
-}
-
-function renderDaysList(days) {
-    const historyUl = document.querySelector("#history-container")
-    days.forEach(function(day) {
-        const newDate = retrieveJsDate(day)
-        const dayLi = document.createElement("li")
-        dayLi.textContent = newDate
-        historyUl.append(dayLi)
-
-        dayLi.addEventListener("click", function(e) {
-            renderDay(day)
-            currentDay = day
-        })
-
-    })
-}
-
-function retrieveJsDate(day) {
-    const currentDate = day.created_at.split("T")
-    const date = currentDate[0]
-    const newDate = new Date(date.replace(/-/g, '\/'));
-    return newDate.toLocaleDateString("en-US")
-}
-
-function renderDay(day) {
-    const date = retrieveJsDate(day)
-    const dateHeader = document.querySelector("h1")
-    dateHeader.textContent = `${date} Tasks`
-
-    const taskList = document.querySelector("#task-list")
-    taskList.innerHTML = ""
-    //could i store the day.id somewhere when rendering days?
-    day.tasks.forEach(function(task) {
-        renderTask(task)
-    })
-}
-
-function renderTask(task) {
-    const taskList = document.querySelector("#task-list")
-    const taskLi = document.createElement("li")
-    taskLi.innerHTML = `
-    <a href="" class="markComplete"> <i class="fa fa-heart"></i>  </a>
-    <span for="content" class="taskContent" data-id="${task.id}" contenteditable="false"> ${task.content}</span>
-    
-    <button class="deleteButton">delete</button>
-    <br>
-    `
-    taskList.append(taskLi)
-}
-
-function createNewTask() {
-    console.log(currentDay)
-    const newTaskForm = document.querySelector("#new-task-form")
-    newTaskForm.addEventListener("submit", function(e){
-        e.preventDefault();
-
-        const categoryOption = newTaskForm.querySelector("select")
-        const chosenOption = categoryOption.options[categoryOption.selectedIndex]
-        const categoryId = chosenOption.getAttribute("data-id")
-
-        const newTaskObj = {
-            category_id: categoryId,
-            day_id: currentDay.id,
-            content: newTaskForm.content.value,
-        }
-        console.log(newTaskObj)
-        createTaskReq(newTaskObj)
-    })
-}
-
 function createTaskReq(newTaskObj) {
     const config = {
         method: "POST",
@@ -134,52 +51,6 @@ function createTaskReq(newTaskObj) {
     .then(function(newTask){
         console.log(newTask)
         renderTask(newTask)
-    })
-
-}
-
-//delete task
-function deleteEditTask() {
-    const taskList = document.querySelector("#task-list")
-    taskList.addEventListener("click", function(e){
-
-        if (e.target.className === "deleteButton") {
-            const task = e.target.previousElementSibling
-            const taskId = task.getAttribute('data-id');
-            deleteTaskReq(taskId, e.target.parentElement)
-
-        }
-        else if (e.target.className === "taskContent") {
-            const task = e.target
-            task.contentEditable = "true";
-
-            task.addEventListener("keypress", function(e){
-                e.stopImmediatePropagation()
-                if (e.key === "Enter") {
-                    e.preventDefault()
-                    const id = task.getAttribute('data-id')
-
-                    const patchObj = {
-                        content: task.textContent
-                    }
-                    editTaskRequest(id, patchObj)
-                }
-            })
-        }
-
-        else if (e.target.className === "markComplete") {
-            e.preventDefault()
-            console.log(e.target)
-            const completeStatus = e.target 
-            const id = completeStatus.nextElementSibling.getAttribute('data-id')
-
-            patchObj = {
-                complete: true
-            }
-
-            markCompleteReq(id, patchObj)
-            
-        }
     })
 }
 
@@ -221,7 +92,7 @@ function editTaskRequest(id, taskObj) {
     })
 }
 
-function markCompleteReq(id, taskObj) {
+function markCompleteReq(id, taskObj, heart) {
     const config = {
         method: "PATCH",
         headers: {
@@ -237,6 +108,157 @@ function markCompleteReq(id, taskObj) {
     })
     .then(function(newTask){
         console.log(newTask)
-        
+        likeStyleHelper(heart, newTask)
     })
+}
+
+///** DOM RENDER FUNCTIONS **///
+
+function renderCategories(categories) {
+    const categorySelect = document.querySelector("#category")
+    categories.forEach(function(category){
+        const categoryOption = document.createElement("option")
+        categoryOption.textContent = category.name
+        categoryOption.setAttribute("data-id" , `${category.id}`);
+        categorySelect.append(categoryOption)
+    })
+}
+
+function renderDaysList(days) {
+    const historyUl = document.querySelector("#history-container")
+    days.forEach(function(day) {
+        const newDate = retrieveJsDate(day)
+        const dayLi = document.createElement("li")
+        dayLi.textContent = newDate
+        historyUl.append(dayLi)
+
+        dayLi.addEventListener("click", function(e) {
+            renderDay(day)
+            currentDay = day
+        })
+
+    })
+}
+
+function renderDay(day) {
+    const date = retrieveJsDate(day)
+    const dateHeader = document.querySelector("h1")
+    dateHeader.textContent = `${date} Tasks`
+
+    const taskList = document.querySelector("#task-list")
+    taskList.innerHTML = ""
+    //could i store the day.id somewhere when rendering days?
+    day.tasks.forEach(function(task) {
+        renderTask(task)
+    })
+}
+
+function renderTask(task) {
+    const taskList = document.querySelector("#task-list")
+    const taskLi = document.createElement("li")
+    taskLi.innerHTML = `
+    <a href="" class="markComplete"> <i class="fa fa-heart"></i>  </a>
+    <span for="content" class="taskContent" data-id="${task.id}" contenteditable="false"> ${task.content}</span>
+    
+    <button class="deleteButton">delete</button>
+    <br>
+    <span>${task.category.name}</span>
+    <img class="categoryImg" src="${task.category.icon}" alt="${task.category.name} icon">
+    `
+    taskList.append(taskLi)
+    console.log(task.category)
+    const heartIcon = taskLi.querySelector("i")
+    likeStyleHelper(heartIcon, task)
+}
+
+function createNewTask() {
+    console.log(currentDay)
+    const newTaskForm = document.querySelector("#new-task-form")
+    newTaskForm.addEventListener("submit", function(e){
+        e.preventDefault();
+
+        const categoryOption = newTaskForm.querySelector("select")
+        const chosenOption = categoryOption.options[categoryOption.selectedIndex]
+        const categoryId = chosenOption.getAttribute("data-id")
+
+        const newTaskObj = {
+            category_id: categoryId,
+            day_id: currentDay.id,
+            content: newTaskForm.content.value,
+        }
+        console.log(newTaskObj)
+        createTaskReq(newTaskObj)
+    })
+}
+
+//delete task
+function deleteEditTask() {
+    const taskList = document.querySelector("#task-list")
+    taskList.addEventListener("click", function(e){
+
+        if (e.target.className === "deleteButton") {
+            const task = e.target.previousElementSibling
+            const taskId = task.getAttribute('data-id');
+            deleteTaskReq(taskId, e.target.parentElement)
+
+        }
+        else if (e.target.className === "taskContent") {
+            const task = e.target
+            task.contentEditable = "true";
+
+            task.addEventListener("keypress", function(e){
+                e.stopImmediatePropagation()
+                if (e.key === "Enter") {
+                    e.preventDefault()
+                    const id = task.getAttribute('data-id')
+
+                    const patchObj = {
+                        content: task.textContent
+                    }
+                    editTaskRequest(id, patchObj)
+                }
+            })
+        }
+
+        else if (e.target.className === "markComplete") {
+            e.preventDefault()
+            console.log(e.target)
+            const completeStatus = e.target 
+            const id = completeStatus.nextElementSibling.getAttribute('data-id')
+            const heart = e.target.querySelector("i")
+
+            patchObj = {
+                complete: findCompleteStatus(heart),
+            } 
+            console.log(patchObj)
+            markCompleteReq(id, patchObj, heart)
+        }
+    })
+}
+
+///** HELPERS **///
+
+function retrieveJsDate(day) {
+    const currentDate = day.created_at.split("T")
+    const date = currentDate[0]
+    const newDate = new Date(date.replace(/-/g, '\/'));
+    return newDate.toLocaleDateString("en-US")
+}
+
+function likeStyleHelper(heart, task) {
+    if (task.complete === true) {
+        heart.style.color = "black"
+    }
+    else if (task.complete === false) {
+        heart.style.color = "red"
+    }
+}
+
+function findCompleteStatus(heart) {
+    if (heart.style.color === "black") {
+        return false
+    }
+    else if (heart.style.color = "red") {
+        return true
+    }
 }
